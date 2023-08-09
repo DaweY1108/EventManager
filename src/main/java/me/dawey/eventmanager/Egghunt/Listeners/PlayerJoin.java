43afaeb891033b4e19d75130a0ffce55e1cbb551
Dominik <csepy.dominik@gmail.com>
2023-08-09 08:41:25 +0200

    Update PlayerJoin.java
    Removed onPlayerJoin event

--- src/main/java/me/dawey/eventmanager/Egghunt/Listeners/PlayerJoin.java
+++ src/main/java/me/dawey/eventmanager/Egghunt/Listeners/PlayerJoin.java
@@ -13,13 +13,13 @@ public class PlayerJoin implements Listener {
     public PlayerJoin(EventManager plugin) {
         this.plugin = plugin;
     }
-    @EventHandler
+    /*@EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         if (plugin.getEgghunt().isStarted) {
             event.getPlayer().sendMessage(ColorFormat.format(
                     plugin.getEgghunt().getData().getEventPrefix() + "A húsvéti event elérhető eddig: &a" + plugin.getConfig().getString("egghunt.end-date") + " &7A jutalmat a tojásokra való jobklikkeléssel kapod meg!"
             ));
         }
-    }
+    }*/
 
 }
